479963e2eed7e3d7b39461a4c379409c3671e2da
Rustam <imankulov.ru@gmail.com>
2022-09-04 19:09:51 +0300

    Task 13

---
+++ Desktop/GB.ru/Seminar_02_09_22/Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("ВВЕДИТЕ ЦЕЛОЕ  ЧИСЛО");
+string number = Console.ReadLine();
+
+if(number.Length >= 3){
+int newNumber = Convert.ToInt32(number.Substring(0, 3));
+int thirdNumber = newNumber % 10;
+Console.WriteLine(thirdNumber);
+} else {
+    Console.WriteLine("Третьей цифры нет");
+}
