8e97f2aab7ce006352491ae34d6232acee861f8c
Rustam <imankulov.ru@gmail.com>
2022-09-04 19:16:01 +0300

    Task15

---
+++ Desktop/GB.ru/Seminar_02_09_22/Task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("ВВЕДИТЕ ЦЕЛОЕ ЧИСЛО");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 6 || number == 7){
+Console.WriteLine("да");
+} else {
+    Console.WriteLine("нет");
+}
